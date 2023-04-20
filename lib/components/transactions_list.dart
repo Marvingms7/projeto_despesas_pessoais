60d4e9466e5a1739fe409be273cb54b71ed2519b
Marcus Vinicius 07 <marcus.vinicius.16041997@gmail.com>
2023-04-20 16:41:46 -0300

    Update transactions_list.dart

--- lib/components/transactions_list.dart
+++ lib/components/transactions_list.dart
@@ -11,51 +11,49 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 300,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions.map((tr) {
-            return Card(
-              child: Row(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.symmetric(
-                        horizontal: 15, vertical: 10),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.purple, width: 2),
+      child: ListView(
+        children: transactions.map((tr) {
+          return Card(
+            child: Row(
+              children: [
+                Container(
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.purple, width: 2),
+                  ),
+                  padding: const EdgeInsets.all(10),
+                  child: Text(
+                    'R\$${tr.value}',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
                     ),
-                    padding: const EdgeInsets.all(10),
-                    child: Text(
-                      'R\$${tr.value}',
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      tr.title,
                       style: const TextStyle(
                         fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple,
+                        fontSize: 16,
                       ),
                     ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        tr.title,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),
+                    Text(
+                      DateFormat('d MMM y').format(tr.data),
+                      style: const TextStyle(
+                        color: Colors.grey,
                       ),
-                      Text(
-                        DateFormat('d MMM y').format(tr.data),
-                        style: const TextStyle(
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            );
-          }).toList(),
-        ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        }).toList(),
       ),
     );
   }
