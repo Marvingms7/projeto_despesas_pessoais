a086c08cec9c793ddbf8fc1f10b6076981f59b63
Marcus Vinicius 07 <marcus.vinicius.16041997@gmail.com>
2023-04-27 17:06:37 -0300

    Chart
    Começando a criar o chart

---
+++ lib/components/chart.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Chart extends StatelessWidget {
+  const Chart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+    );
+  }
+}
