40fe0a4a337e7b0e33267f632e0ac950c3c0a613
Marcus Vinicius 07 <marcus.vinicius.16041997@gmail.com>
2023-04-27 17:25:21 -0300

    Update chart.dart

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:projeto_despesas_pessoais/models/transaction.dart';
 
 class Chart extends StatelessWidget {
-  const Chart({super.key});
+  const Chart({super.key, required this.recentTransaction});
+
+  final List<Transaction> recentTransaction;
+
+  List<Map<String, Object>> get groupedTransactions {
+    return List.generate(7, (index) {
+      return {'day': 'T', 'value': 59.50};
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
