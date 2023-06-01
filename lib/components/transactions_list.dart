2ea605d57c2513de80188fe64c83ca36f5a4e206
Marcus Vinicius 07 <marcus.vinicius.16041997@gmail.com>
2023-06-01 14:50:25 -0300

    Update transactions_list.dart

--- lib/components/transactions_list.dart
+++ lib/components/transactions_list.dart
@@ -13,27 +13,29 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return transactions.isEmpty
-        ? Column(
-            children: [
-              const SizedBox(
-                height: 20,
-              ),
-              Text(
-                'Nenhuma transação cadastrada',
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              SizedBox(
-                height: 200,
-                child: Image.asset(
-                  'assets/images/waiting.png',
-                  fit: BoxFit.cover,
+        ? LayoutBuilder(builder: (ctx, constraints) {
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 20,
                 ),
-              )
-            ],
-          )
+                Text(
+                  'Nenhuma transação cadastrada',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  height: constraints.maxHeight * 0.6,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
+                  ),
+                )
+              ],
+            );
+          })
         : ListView.builder(
             itemCount: transactions.length,
             itemBuilder: (ctx, index) {
