8ca3a817fc5ad329b86720021d50e16841fd3dd8
Marcus Vinicius 07 <marcus.vinicius.16041997@gmail.com>
2023-06-09 09:54:41 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -98,7 +98,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    bool isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
+    bool isLandscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
 
     final appBar = AppBar(
       title: const Text(
@@ -106,6 +107,15 @@ class _MyHomePageState extends State<MyHomePage> {
         style: TextStyle(color: Colors.white),
       ),
       actions: [
+        if (isLandscape)
+          IconButton(
+            icon: Icon(_showChart ? Icons.list : Icons.show_chart_rounded),
+            onPressed: () {
+              setState(() {
+                _showChart = !_showChart;
+              });
+            },
+          ),
         IconButton(
           icon: const Icon(Icons.add),
           onPressed: () => _openTransactionFormModal(context),
@@ -122,27 +132,29 @@ class _MyHomePageState extends State<MyHomePage> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             if (isLandscape)
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('Exibir gráfico'),
-                Switch(value: _showChart, onChanged: (value) {
-                  setState(() {
-                    _showChart = value;
-                  });
-                }),
-              ],
-            ),
-            if(_showChart || !isLandscape)
-            SizedBox(
-              height: availabelHeight * (isLandscape ? 0.7 : 0.3),
-              child: Chart(_recentTransaction),
-            ),
-            if(!_showChart || !isLandscape)
-            SizedBox(
-              height: availabelHeight * 0.7,
-              child: TransactionList(_transactions, _removeTransaction),
-            ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Exibir gráfico'),
+                  Switch(
+                      value: _showChart,
+                      onChanged: (value) {
+                        setState(() {
+                          _showChart = value;
+                        });
+                      }),
+                ],
+              ),
+            if (_showChart || !isLandscape)
+              SizedBox(
+                height: availabelHeight * (isLandscape ? 0.7 : 0.3),
+                child: Chart(_recentTransaction),
+              ),
+            if (!_showChart || !isLandscape)
+              SizedBox(
+                height: availabelHeight * 0.7,
+                child: TransactionList(_transactions, _removeTransaction),
+              ),
           ],
         ),
       ),
