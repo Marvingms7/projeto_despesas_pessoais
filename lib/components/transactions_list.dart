dfe1ae28bfd49a7938e13f4f680a18c08f72a493
Marcus Vinicius 07 <marcus.vinicius.16041997@gmail.com>
2023-06-15 12:01:41 -0300

    Refatorando transaction item

--- lib/components/transactions_list.dart
+++ lib/components/transactions_list.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'package:projeto_despesas_pessoais/components/transaction_item.dart';
 import '../models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
@@ -40,40 +40,7 @@ class TransactionList extends StatelessWidget {
             itemCount: transactions.length,
             itemBuilder: (ctx, index) {
               final tr = transactions[index];
-              return Card(
-                elevation: 5,
-                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                child: ListTile(
-                  leading: CircleAvatar(
-                    backgroundColor: Colors.deepPurple,
-                    radius: 30,
-                    child: Padding(
-                      padding: const EdgeInsets.all(6),
-                      child: FittedBox(
-                        child: Text('R\$${tr.value}'),
-                      ),
-                    ),
-                  ),
-                  title: Text(
-                    tr.title,
-                    style: Theme.of(context).textTheme.titleLarge,
-                  ),
-                  subtitle: Text(DateFormat('d MMM y').format(tr.data)),
-                  trailing: MediaQuery.of(context).size.width > 490
-                      ? TextButton.icon(
-                          onPressed: () => onRemove(tr.id),
-                          icon:
-                              const Icon(Icons.delete, color: Colors.redAccent),
-                          label: const Text('Remover',
-                              style: TextStyle(color: Colors.redAccent)),
-                        )
-                      : IconButton(
-                          icon: const Icon(Icons.delete),
-                          color: Theme.of(context).colorScheme.error,
-                          onPressed: () => onRemove(tr.id),
-                        ),
-                ),
-              );
+              return TransactionItem(tr: tr, onRemove: onRemove);
             },
           );
   }
